18c761b28ae93d535191e2d9ce128ac255044e30
Nick Eldridge <nicholas.eldridge@gmail.com>
2018-05-17 20:19:15 -0400

    removing unnecessary backend, moving backend testing to a different project type.

--- src/components/FloppyForm/FloppyForm.js
+++ src/components/FloppyForm/FloppyForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FloppyText from '../FloppyText/FloppyText'
 
 export default class FloppyForm extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -31,26 +32,19 @@ export default class FloppyForm extends Component {
 
         let state = this.state
         state[name] = value
-        let url = 'http://localhost:5000/api/v1.0/floppies/' + state.data_size + '/' + state.data_unit;
 
-        fetch(url)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                this.updateFloppies(data['floppies'])
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-        
+        let modifiers = {
+            'KB': Math.pow(parseFloat(10), parseFloat(-3)),
+            'MB': 1,
+            'GB': Math.pow(parseFloat(10), parseFloat(3)),
+            'TB': Math.pow(parseFloat(10), parseFloat(6)),
+            'PB': Math.pow(parseFloat(10), parseFloat(9)),
+        };
+
+        state.floppies = parseInt(Math.ceil(parseFloat(modifiers[state.data_unit]) * parseFloat(state.data_size) / parseFloat(1.44)));
         this.setState(state);
     }
     
-    updateFloppies(floppies) {
-        this.setState({'floppies': floppies})
-    }
-
     render() {
         return (
         <form onSubmit={this.handleSubmit}>
